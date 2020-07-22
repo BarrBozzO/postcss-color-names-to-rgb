aea58827606367c1577ff494ef5cbea72bdda250
Constantine <barr.develop@gmail.com>
2020-07-22 15:15:53 +0400

    fix decls filter value

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let postcss = require("postcss");
 module.exports = postcss.plugin("postcss-first-plugin", (opts = {}) => {
   return (root) => {
     root.walkRules((rule) => {
-      rule.walkDecls("color:", (decl) => {
+      rule.walkDecls("color", (decl) => {
         if (decl.value === "red") {
           decl.replaceWith(decl.clone({ value: "rgb(255,0,0)" }));
         }
